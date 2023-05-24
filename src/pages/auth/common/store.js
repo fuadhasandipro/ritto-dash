168562da024af6e58743245f84c0b79fee6db878
fuadhasan08 <fuadhasandipro08@gmail.com>
2023-05-24 23:05:27 +0600

    auth changed

--- src/pages/auth/common/store.js
+++ src/pages/auth/common/store.js
@@ -1,29 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
-import { toast } from "react-toastify";
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+import { toast } from 'react-toastify';
 
 const initialUsers = () => {
-  const item = window.localStorage.getItem("users");
+  const item = window.localStorage.getItem('users');
   return item
     ? JSON.parse(item)
     : [
         {
           id: uuidv4(),
-          name: "dashcode",
-          email: "dashcode@gmail.com",
-          password: "dashcode",
+          name: 'ritto360',
+          email: 'ritto360@gmail.com',
+          password: 'ritto360',
         },
       ];
 };
 // save users in local storage
 
 const initialIsAuth = () => {
-  const item = window.localStorage.getItem("isAuth");
+  const item = window.localStorage.getItem('isAuth');
   return item ? JSON.parse(item) : false;
 };
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState: {
     users: initialUsers(),
     isAuth: initialIsAuth(),
@@ -33,15 +33,15 @@ export const authSlice = createSlice({
       const { name, email, password } = action.payload;
       const user = state.users.find((user) => user.email === email);
       if (user) {
-        toast.error("User already exists", {
-          position: "top-right",
+        toast.error('User already exists', {
+          position: 'top-right',
           autoClose: 1500,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "light",
+          theme: 'light',
         });
       } else {
         state.users.push({
@@ -50,16 +50,16 @@ export const authSlice = createSlice({
           email,
           password,
         });
-        window.localStorage.setItem("users", JSON.stringify(state.users));
-        toast.success("User registered successfully", {
-          position: "top-right",
+        window.localStorage.setItem('users', JSON.stringify(state.users));
+        toast.success('User registered successfully', {
+          position: 'top-right',
           autoClose: 1500,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "light",
+          theme: 'light',
         });
       }
     },
@@ -67,24 +67,24 @@ export const authSlice = createSlice({
     handleLogin: (state, action) => {
       state.isAuth = action.payload;
       // save isAuth in local storage
-      window.localStorage.setItem("isAuth", JSON.stringify(state.isAuth));
-      toast.success("User logged in successfully", {
-        position: "top-right",
+      window.localStorage.setItem('isAuth', JSON.stringify(state.isAuth));
+      toast.success('User logged in successfully', {
+        position: 'top-right',
         autoClose: 1500,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "light",
+        theme: 'light',
       });
     },
     handleLogout: (state, action) => {
       state.isAuth = action.payload;
       // remove isAuth from local storage
-      window.localStorage.removeItem("isAuth");
-      toast.success("User logged out successfully", {
-        position: "top-right",
+      window.localStorage.removeItem('isAuth');
+      toast.success('User logged out successfully', {
+        position: 'top-right',
       });
     },
   },
